9256d08824496c5a3b794cbcb2bcdc76fd6edf51
idevm <77122241+idevm@users.noreply.github.com>
2021-10-17 21:43:43 +0300

    added functions: add, find

--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace BirthdaysReminder
 {
+    public enum State
+    {
+        today,
+        thisMonth,
+        thisYear,
+        addingName,
+        addingBirthday,
+        addingConfirm,
+        addingComplete,
+        removingByName,
+        removingConfirm,
+        removingComplete,
+        finding,
+        findingComplete
+    }
+
     public partial class Form1 : Form
     {
         public Form1(BDapp app)
@@ -13,100 +30,218 @@ namespace BirthdaysReminder
 
         public BDapp app;
 
+        public State state = State.today;
+
         public string tName = "";
 
         public string tBD = "";
 
-        private void ModeButton_Click(object sender, EventArgs e)
+
+        private void PreviousButton_Click(object sender, EventArgs e)
         {
-            if (this.ModeButton.Text == "В этом месяце")
+            switch (state)
             {
-                Program.ThisMonth(this);
-                this.ModeButton.Text = "Все";
-            }
-            else if (this.ModeButton.Text == "Сегодня" || this.ModeButton.Text == "ОК")
-            {
-                Program.Today(this);
-                this.ModeButton.Text = "В этом месяце";
-            }
-            else if (this.ModeButton.Text == "Все")
-            {
-                Program.ThisYear(this);
-                this.ModeButton.Text = "Сегодня";
-            }
-            else if (this.ModeButton.Text == "Отменить")
-            {
-                this.ModeButton.Text = "В этом месяце";
-                Program.Today(this);
-                this.AddBDButton.Text = "Добавить ДР";
-                textBox1.Text = "";
-                textBox1.Enabled = false;
-            }
-            else if (this.ModeButton.Text == "Назад ")
-            {
-                textBox1.Focus();
-                label1.Text = "Введите ФИО";
-                this.AddBDButton.Text = "Далее";
-                this.ModeButton.Text = "Отменить";
-                textBox1.Text = "";
+                case State.today:
+                    break;
+                case State.thisMonth:
+                    break;
+                case State.thisYear:
+                    break;
+                case State.addingName:
+                    state = State.today;
+                    PreviousButton.Visible = false;
+                    NextButton.Visible = false;
+                    inputBox1.Visible = false;
+                    Program.Today(this);
+                    break;
+                case State.addingBirthday:
+                    state = State.addingName;
+                    PreviousButton.Text = "Отменить";
+                    NextButton.Text = "Далее";
+                    inputBox1.Visible = true;
+                    inputBox1.Focus();
+                    label1.Text = "Введите ФИО";
+                    Text = "Добавление данных";
+                    break;
+                case State.addingConfirm:
+                    state = State.today;
+                    PreviousButton.Visible = false;
+                    NextButton.Visible = false;
+                    inputBox1.Visible = false;
+                    Program.Today(this);
+                    break;
+                case State.addingComplete:
+                    state = State.today;
+                    PreviousButton.Visible = false;
+                    NextButton.Visible = false;
+                    inputBox1.Visible = false;
+                    Program.Today(this);
+                    break;
+                case State.removingByName:
+                    break;
+                case State.removingConfirm:
+                    break;
+                case State.removingComplete:
+                    break;
+                case State.finding:
+                    state = State.today;
+                    PreviousButton.Visible = false;
+                    NextButton.Visible = false;
+                    inputBox1.Visible = false;
+                    Program.Today(this);
+                    break;
+                case State.findingComplete:
+                    break;
             }
         }
 
 
-        private void AddBDButton_Click(object sender, EventArgs e)
+        private void NextButton_Click(object sender, EventArgs e)
         {
-            if (this.AddBDButton.Text == "Добавить ДР")
+            switch (state)
             {
-                textBox1.Enabled = true;
-                textBox1.Focus();
-                label1.Text = "Введите ФИО";
-                this.AddBDButton.Text = "Далее";
-                this.ModeButton.Text = "Отменить";
-                Text = "Добавление данных";
+                case State.today:
+                    break;
+                case State.thisMonth:
+                    break;
+                case State.thisYear:
+                    break;
+                case State.addingName:
+                    inputBox1.Focus();
+                    tName = inputBox1.Text;
+                    if (app.ValidInput(name: tName))
+                    {
+                        state = State.addingBirthday;
+                        label1.Text = "Введите дату рождения\n(в формате ДД.ММ.ГГГГ)";
+                        inputBox1.Text = "";
+                        NextButton.Text = "Далее ";
+                        PreviousButton.Text = "Назад ";
+                    }
+                    else
+                    {
+                        label1.Text = "Введите корректные ФИО\n(например: Иванов Иван Иванович)";
+                        inputBox1.Focus();
+                    }
+                    break;
+                case State.addingBirthday:
+                    tBD = inputBox1.Text;
+                    if (app.ValidInput(birthday: tBD))
+                    {
+                        state = State.addingConfirm;
+                        label1.Text = $"Сохранить \"{tName} ({tBD})\"?";
+                        NextButton.Text = "Сохранить";
+                        PreviousButton.Text = "Отменить";
+                        inputBox1.Text = "";
+                    }
+                    else
+                    {
+                        label1.Text = "Введите корректную дату рождения в формате ДД.ММ.ГГГГ\n(например: 02.08.1999)";
+                        inputBox1.Focus();
+                    }
+                    break;
+                case State.addingConfirm:
+                    state = State.addingComplete;
+                    app.Persons = app.AddPerson(app.Persons, tName, tBD);
+                    app.WriteFile("db.csv", app.UpdateText(app.Persons));
+                    NextButton.Text = "Добавить еще";
+                    PreviousButton.Text = "ОК";
+                    label1.Text = $"Сохранено: \"{ tName} ({ tBD})\"";
+                    break;
+                case State.addingComplete:
+                    state = State.addingName;
+                    PreviousButton.Text = "Отменить";
+                    NextButton.Text = "Далее";
+                    inputBox1.Focus();
+                    label1.Text = "Введите ФИО";
+                    Text = "Добавление данных";
+                    break;
+                case State.removingByName:
+                    break;
+                case State.removingConfirm:
+                    break;
+                case State.removingComplete:
+                    break;
+                case State.finding:
+                    string txt = inputBox1.Text;
+                    app.Text = app.GetText(app.FindPerson(app.Persons, txt), Mode.findResults);
+                    label1.Text = app.Text;
+                    Text = "Результат поиска";
+                    break;
+                case State.findingComplete:
+                    break;
             }
-            else if (this.AddBDButton.Text == "Далее")
-            {
-                textBox1.Focus();
-                tName = textBox1.Text;
-                if (app.ValidInput(name: tName))
-                {
-                    label1.Text = "Введите дату рождения\n(в формате ДД.ММ.ГГГГ)";
-                    textBox1.Text = "";
-                    this.AddBDButton.Text = "Далее ";
-                    this.ModeButton.Text = "Назад ";
-                }
-                else
-                {
-                    label1.Text = "Введите корректные ФИО\n(например: Иванов Иван Иванович)";
-                    textBox1.Focus();
-                }
-            }
-            else if (this.AddBDButton.Text == "Далее ")
-            {
-                tBD = textBox1.Text;
-                if (app.ValidInput(birthday: tBD))
-                {
-                    label1.Text = $"Сохранить \"{tName} ({tBD})\"?";
-                    this.AddBDButton.Text = "Сохранить";
-                    this.ModeButton.Text = "Отменить";
-                    textBox1.Text = "";
-                    textBox1.Enabled = false;
-                }
-                else
-                {
-                    label1.Text = "Введите корректную дату рождения в формате ДД.ММ.ГГГГ\n(например: 02.08.1999)";
-                    textBox1.Focus();
-                }
-            }
-            else if (this.AddBDButton.Text == "Сохранить")
-            {
-                app.Persons = app.AddPerson(app.Persons, tName, tBD);
-                app.WriteFile("db.csv", app.UpdateText(app.Persons));
-                this.AddBDButton.Text = "Добавить ДР";
-                this.ModeButton.Text = "ОК";
-                this.ModeButton.Enabled = true;
-                label1.Text = $"Сохранено: \"{ tName} ({ tBD})\"";
-            }
+        }
+
+
+        private void ToolStripMenuItemToday_Click(object sender, EventArgs e)
+        {
+            state = State.today;
+            PreviousButton.Visible = false;
+            NextButton.Visible = false;
+            inputBox1.Visible = false;
+            Program.Today(this);
+        }
+
+
+        private void ToolStripMenuItemThisMonth_Click(object sender, EventArgs e)
+        {
+            state = State.thisMonth;
+            PreviousButton.Visible = false;
+            NextButton.Visible = false;
+            inputBox1.Visible = false;
+            Program.ThisMonth(this);
+        }
+
+
+        private void ToolStripMenuItemAThisYear_Click(object sender, EventArgs e)
+        {
+            state = State.thisYear;
+            PreviousButton.Visible = false;
+            NextButton.Visible = false;
+            inputBox1.Visible = false;
+            Program.ThisYear(this);
+        }
+
+
+        private void ToolStripMenuItemFind_Click(object sender, EventArgs e)
+        {
+            state = State.finding;
+            PreviousButton.Visible = true;
+            PreviousButton.Text = "Отменить";
+            NextButton.Visible = true;
+            NextButton.Text = "Найти";
+            inputBox1.Visible = true;
+            inputBox1.Focus();
+            label1.Text = "Введите текст для поиска и нажмите кнопку 'Найти'";
+            Text = "Поиск";
+        }
+
+
+        private void ToolStripMenuItemAdd_Click(object sender, EventArgs e)
+        {
+            state = State.addingName;
+            PreviousButton.Visible = true;
+            PreviousButton.Text = "Отменить";
+            NextButton.Visible = true;
+            NextButton.Text = "Далее";
+            inputBox1.Visible = true;
+            inputBox1.Focus();
+            label1.Text = "Введите ФИО";
+            Text = "Добавление данных";
+        }
+
+
+        private void ToolStripMenuItemRemove_Click(object sender, EventArgs e)
+        {
+            state = State.removingByName;
+            PreviousButton.Visible = true;
+            PreviousButton.Text = "Отменить";
+            NextButton.Visible = true;
+            NextButton.Text = "Далее";
+            inputBox1.Visible = true;
+            inputBox1.Focus();
+            label1.Text = "Введите ФИО для удаления";
+            Text = "Удаление данных";
         }
     }
 }
